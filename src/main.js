9d97efa0d1cc0c3114e4bc40432e28816a903c65
CC718 <CC86718@163.com>
2024-05-30 00:03:55 +0800

    axios二次封装,并测试

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+// import router from './router'
+
+//测试接口函数
+import { getCategory } from '@/apis/testAPI.js'
+getCategory().then(res =>
+   console.log(res)
+)
+
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+// app.use(router)
 
 app.mount('#app')
